b3205857a77da461bdb682409d5c06d5af017ba8
Michael E. Karpeles <michael.karpeles@gmail.com>
2013-05-12 22:54:43 -0700

    updated style

--- pymarks/reversing.py
+++ pymarks/reversing.py
@@ -2,15 +2,16 @@
 #-*- coding: utf-8 -*-
 
 """
+    reversing
+    ~~~~~~~~~
+
+    Experimentation to determine the best way to reverse a list (under
+    different circumstances)
 """
 
-import timeit
+from utils import ptimeit
 
 _setup = "ol = [1, 2, 3] * 1000"
-
-def ptimeit(expr, setup="", n=100000, desc=""):
-    t = timeit.timeit(expr, setup=setup, number=n)
-    print "%s: %s loops, best of 3: %s usec per loop" % (desc, n, t)
 
 # I ran this set of 100000 and came up with 11.2:
 ptimeit("reversed(ol)", setup=_setup, desc="reversed")
