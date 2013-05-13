a0bf6c6f443168f7598a6181f1d1b42baf06092a
Michael E. Karpeles <michael.karpeles@gmail.com>
2013-05-12 23:18:44 -0700

    added conditional tuple evaluation

--- pymarks/if.py
+++ pymarks/if.py
@@ -13,6 +13,9 @@ from utils import ptimeit
 # Prereq: Python 2.5+
 ptimeit("x = 5 if False else 4", desc="inline if")
 
+# Slightly less performant than inline or multiline
+ptimeit("x = (5, 4)[False]", desc="tuple evaluation")
+
 ptimeit("x = 5 if False else 4", """if False:
     x = 5
 else:
